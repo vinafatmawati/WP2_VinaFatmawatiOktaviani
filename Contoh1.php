651d00abe336ccb36b5485d70bdb19eaafce346b
vinafatmawati <vinafao27@gmail.com>
2022-09-18 19:47:23 +0700

    penambahan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1> Perkenalkan <h1>";
+    echo "Nama Saya Vina Fatmawati
+          Saya Tinggal di Cengkareng
+          Saya ingin belajar programing";
+    }
+
+}
